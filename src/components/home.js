50b9e8be7f490f53dcddcc8baef279df547037d8
Mustafa Teber <msteber2506@gmail.com>
2024-10-06 00:01:42 -0400

    Initialize project

---
+++ src/components/home.js
@@ -0,0 +1,13 @@
+
+
+const Home = () => {
+
+    return(
+        <>
+        <div className="text-center">
+            <h2>Find a movie to watch tonight</h2>
+            <hr/>
+        </div>
+        </>
+    )
+}
